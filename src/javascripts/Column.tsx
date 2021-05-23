b549c9ffae0d4e3bbe789cc6a4696a63116433ef
br-to <barabara0224@gmail.com>
2021-05-23 14:50:20 +0900

    モックにインタラクションを加える

--- src/javascripts/Column.tsx
+++ src/javascripts/Column.tsx
@@ -1,34 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import * as color from './color';
 import { Card } from './Card';
 import { PlusIcon } from './icon';
+import { InputForm as _InputForm } from './InputForm';
 
 export function Column({
   title,
-  cards,
+  filterValue: rawFilterValue,
+  cards: rawCards,
+  onCardDragStart,
+  onCardDrop,
+  onCardDeleteClick,
 }: {
   title?: string;
+  filterValue?: string;
   cards: {
-    id: any;
+    id: string;
     text?: string;
   }[];
+  onCardDragStart?(id: string): void;
+  onCardDrop?(entered: string | null): void;
+  onCardDeleteClick?(id: string): void;
 }) {
-  const totalCount = cards.length;
+  // rawFilterValueの前後の空白を取り除く
+  const filterValue = rawFilterValue?.trim();
+  // 検索結果空白排除
+  const keywords = filterValue?.toLowerCase().split(/\s+/g) ?? [];
 
+  console.log(`key ${keywords}`);
+  const cards = rawCards.filter(({ text }) =>
+    keywords?.every(word => text?.toLowerCase().includes(word)),
+  );
+
+  // 各column内のcardの個数
+  const totalCount = rawCards.length;
+
+  // inputForm入力あるたびにonChangeに入れたsetTextを呼ぶ
+  // textはColumnコンポーネントのstate
+  const [text, setText] = useState('');
+  // inputMode InputForm の表示・非表示を制御しています。
+  const [inputMode, setInputMode] = useState(false);
+  // 現在の値から次の値を算出する関数を渡す
+  const toggleInput = () => setInputMode((value: boolean) => !value);
+  const confirmInput = () => setText('');
+  const cancelInput = () => setInputMode(false);
+
+  const [draggingCardID, setDraggingCardID] =
+    useState<string | undefined>(undefined);
+
+  const handleCardDragStart = (id: string) => {
+    setDraggingCardID(id);
+    onCardDragStart?.(id);
+  };
   return (
     <Container>
       <Header>
         <CountBadge>{totalCount}</CountBadge>
         <ColumnName>{title}</ColumnName>
 
-        <AddButton />
+        <AddButton onClick={toggleInput} />
       </Header>
 
+      {inputMode && (
+        <InputForm
+          value={text}
+          onChange={setText}
+          onConfirm={confirmInput}
+          onCancel={cancelInput}
+        />
+      )}
+
+      {filterValue && <ResultCount>{cards.length} results</ResultCount>}
+
       <VerticalScroll>
-        {cards.map(({ id, text }) => (
-          <Card text={text} />
+        {cards.map(({ id, text }, i) => (
+          <Card.DropArea
+            key={id}
+            disabled={
+              draggingCardID !== undefined &&
+              (id === draggingCardID || cards[i - 1]?.id === draggingCardID)
+            }
+            onDrop={() => onCardDrop?.(id)}
+          >
+            <Card
+              text={text}
+              onDragStart={() => handleCardDragStart(id)}
+              onDragEnd={() => setDraggingCardID(undefined)}
+              onDeleteClick={() => onCardDeleteClick?.(id)}
+            />
+          </Card.DropArea>
         ))}
+        <Card.DropArea
+          style={{ height: '100%' }}
+          disabled={
+            draggingCardID !== undefined &&
+            cards[cards.length - 1]?.id === draggingCardID
+          }
+          onDrop={() => onCardDrop?.(null)}
+        />
       </VerticalScroll>
     </Container>
   );
@@ -81,6 +151,17 @@ const AddButton = styled.button.attrs({
   :hover {
     color: ${color.Blue};
   }
+`;
+
+// InputFormのstyleを変えるために_InputFormで呼び出している
+const InputForm = styled(_InputForm)`
+  padding: 8px;
+`;
+
+const ResultCount = styled.div`
+  color: ${color.Black};
+  font-size: 12px;
+  text-align: center;
 `;
 
 const VerticalScroll = styled.div`
